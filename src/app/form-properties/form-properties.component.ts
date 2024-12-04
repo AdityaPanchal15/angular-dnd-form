68d10f797e9a89eba393a2939fe6b406dea1f526
Aditya Panchal <adpanchal15@gmail.com>
2024-12-04 09:49:50 +0530

    Add radio and checkbox group input.

--- src/app/form-properties/form-properties.component.ts
+++ src/app/form-properties/form-properties.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-properties',
@@ -12,6 +12,8 @@ export class FormPropertiesComponent {
    */
   @Input() properties?: any;
   @Input() listingForm!: FormGroup;
+  
+  constructor(private fb: FormBuilder) {}
 
   ngOnChanges(changes: any) {
     if (this.properties) {
@@ -43,12 +45,43 @@ export class FormPropertiesComponent {
       maximum: this.properties?.formValidations?.maximum || '',
       regex: this.properties?.formValidations?.regex || '',
     });
+    
+    if(this.properties?.type === 'radio') {
+      this.listingForm.get('name')?.disable();
+    }
+    
+    this.getOptions.clear(); // Clear existing form groups if necessary
+    
+    this.properties.options.forEach((option: any) => {
+      this.getOptions.push(
+        this.fb.group({
+          id: option.id,
+          label: option.label,
+          value: option.value,
+        })
+      );
+    });
   }
 
-    /**
+  /**
    * Method to get the formValidations form
    */
-    get getFormValidations() {
-      return this.listingForm.get('formValidations') as FormGroup;
-    }
+  get getFormValidations() {
+    return this.listingForm.get('formValidations') as FormGroup;
+  }
+  
+  /**
+   * Method to get options for checkbox/radio group
+   */
+  get getOptions(): FormArray {
+    return this.listingForm.get('options') as FormArray;
+  }
+
+  /**
+   * Remove option from checkbox/radio group
+   * @param index 
+   */
+  removeOption(index: number) {
+    this.getOptions.removeAt(index);
+  }
 }
